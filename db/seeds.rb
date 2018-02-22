37aefbeaa9d18939990c4e541b161d6660010cd8
Matt Roach <mattroach@mac.com>
2018-02-22 17:01:17 +0100

    ex1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,22 @@
-puts 'Cleaning database...'
+require 'open-uri'
+
+puts "creating seeds!"
+
 Cocktail.destroy_all
+Dose.destroy_all
 Ingredient.destroy_all
+
 
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 
 
-puts 'Creating cocktails...'
-cocktails_attributes = [
-  {
-    name: "Gin & Tonic",
-      { dose:         "Gin",
-        ingredient:   "2ml"
-      }
-      { dose:         "Tonic",
-        ingredient:   "50ml"
-      }
-      { dose:         "Lemon",
-        ingredient:   "1 piece"
-      }
-  },
-]
-Cocktail.create!(cocktails_attributes)
-puts 'Finished!'
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks_file = open(url).read
+drinks = JSON.parse(drinks_file)["drinks"]
+drinks.each do |ingredient|
+  Ingredient.create(name: ingredient[ 'strIngredient1' ])
+end
 
+puts "finished seeds"
